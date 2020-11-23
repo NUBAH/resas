dc59f6b96a81465722c8dd6abb10326f44930279
NUBAH <v.nobu227@gmail.com>
2020-11-23 14:38:49 +0900

    add style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from "react-router-dom";
-import Top from './components/Top';
-import population from './components/population';
+import './App.css'
 import axios from 'axios';
 
 const App = props => {
@@ -33,12 +27,12 @@ const App = props => {
     let newRows = [];
     for(var x = rowCount - 1; x >= 0; x--) {
       newRows.push(
-        <tr>
-          <td>{year[0].data[x].year}</td>
-          <td>{year[0].data[x].value} - {x > 0 ? (year[0].data[x].value / year[0].data[x-1].value * 10).toFixed(1) : ""}</td>
-          <td>{year[1].data[x].value} - {year[1].data[x].rate}</td>
-          <td>{year[2].data[x].value} - {year[2].data[x].rate}</td>
-          <td>{year[3].data[x].value} - {year[3].data[x].rate}</td>
+        <tr className="table-row">
+          <td className="year-column">{year[0].data[x].year}</td>
+          <td>{year[0].data[x].value} - ({x > 0 ? (year[0].data[x].value / year[0].data[x-1].value * 10).toFixed(1) : ""})</td>
+          <td>{year[1].data[x].value} - ({year[1].data[x].rate})</td>
+          <td>{year[2].data[x].value} - ({year[2].data[x].rate})</td>
+          <td>{year[3].data[x].value} - ({year[3].data[x].rate})</td>
         </tr>
       );
     }
@@ -48,50 +42,45 @@ const App = props => {
   const handleClick = prefCode =>  {
     axios.get(populationUrl+prefCode, {headers: headers})
       .then(response => {
-        console.log(response);
         setYear(response.data.result.data);
       })
   }
 
     return (
-      <div>
-        <Router>
-            <ul>
-              <li><Link to="/">トップ</Link></li>
-              <li><Link to="/population">人口</Link></li>
-            </ul>
-            <Route path="/" exact component={Top} />
-            <Route path="/population/" exact component={population} />
+      <div className="App">
+        <div className="App-header">
           <h1>都道府県人口構成</h1>
-            <div>
-              {prefList.map((listItem, index) => (
-                <label key={index}>
-                  <input 
-                    type="checkbox" 
-                    onChange={() => handleClick(listItem.prefCode)}
-                    />
-                  {listItem.prefName}
-                </label>
-                )
-              )
-            }
-            </div>
-            <div>
-              <table>
-                <thead>
-                <tr>
-                  <th>年</th>
+        </div>
+        <div className="App-checkbox">
+          {prefList.map((listItem, index) => (
+            <label className="checkbox-label" key={index}>
+              <input 
+                className="checkbox-button"
+                type="checkbox" 
+                onChange={() => handleClick(listItem.prefCode)}
+                />
+              {listItem.prefName}
+            </label>
+            )
+          )
+        }
+        </div>
+        <div className="App-table">
+          <h3>人口構成表</h3>
+          <table className="g-table">
+            <thead>
+              <tr className="table-row">
+                <th>年</th>
                 {year.map((yearItem) => (
-                  <th>{yearItem.label}</th>
+                  <th>{yearItem.label} - (割合)</th>
                 ))}
-                </tr>
-                </thead>
-                <tbody>
-                  {rows}
-                </tbody>
-              </table>
-            </div>
-        </Router>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
 }
